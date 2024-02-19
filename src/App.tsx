d10e81339e7367f9bb9ab2d0abe44b9ed0e8c485
Exanite <exanite78012@gmail.com>
2024-02-18 20:52:53 -0500

    Use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "@website/styles/globals.css";
 import { Layout } from "@website/components/Layout.tsx";
 import { About } from "@website/pages/About.tsx";
 import { Home } from "@website/pages/Home.tsx";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createHashRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route element={<Layout/>}>
       <Route path={"/"} element={<Home/>}/>
