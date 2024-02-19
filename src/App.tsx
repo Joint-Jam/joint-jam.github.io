0d14b4a45b1ad8630bee63f68a7eaeb9f9eccb3b
Exanite <exanite78012@gmail.com>
2024-02-18 20:39:06 -0500

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "@website/styles/globals.css";
 
+import { About } from "@website/pages/About.tsx";
 import { Home } from "@website/pages/Home.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+]);
 
 export const App = () => {
   return (
-    <Home/>
-  )
-}
+    <RouterProvider router={router}/>
+  );
+};
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <>
+      About page. Mainly to test React Router.
+    </>
+  )
+}
