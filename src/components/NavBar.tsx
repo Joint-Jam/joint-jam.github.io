e1473fa158dafb4bd7a2f8ec842c58f2b4f7825b
alectrem <tremblayalec14@gmail.com>
2024-02-25 16:44:16 -0500

    updates nav bar to match mockup

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { faDiscord, faGithub, faItchIo } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logoimg from "@website/assets/jj24logo.png";
 import { PageUrls } from "@website/data/PageUrls.ts";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
@@ -8,7 +9,8 @@ const NavItem = ({ children, to, className = "" }: {children: React.ReactNode; t
   return (
     <Link to={to} className={clsx(
       "whitespace-no-wrap",
-      "mx-2 sm:mx-2 max-sm:my-2",
+      "mx-4 sm:mx-2 max-sm:my-2",
+      "text-3xl",
       className,
     )}>
       {children}
@@ -18,27 +20,26 @@ const NavItem = ({ children, to, className = "" }: {children: React.ReactNode; t
 
 export const NavBar = () => {
   return (
-    <nav>
-      <div className={clsx(
-        "flex flex-col items-center justify-between sm:flex-row sm:items-end",
-        "max-w-screen-md",
-        "mx-auto my-4 px-4",
-        "border-b-2 border-solid border-accent",
-      )}>
-        <div className={"flex-none"}>
-          <NavItem to={PageUrls.home()} className={"text-3xl font-bold"}>JOINT JAM</NavItem>
-        </div>
-        <div className={"flex text-center color-text-shadow"}>
-          <NavItem className="color-text" to={PageUrls.home()}>PAST EVENTS</NavItem>
-          <NavItem className="color-text" to={PageUrls.home()}>PARTICIPATING SCHOOLS</NavItem>
-          <NavItem className="color-text" to={PageUrls.home()}>SPONSORS</NavItem>
-        </div>
-        <div className={"flex-none color-text"}>
-          <NavItem to={"https://github.com/joint-jam"}><FontAwesomeIcon icon={faGithub}/></NavItem>
-          <NavItem to={PageUrls.home()}><FontAwesomeIcon icon={faItchIo}/></NavItem>
-          <NavItem to={PageUrls.home()}><FontAwesomeIcon icon={faDiscord}/></NavItem>
-        </div>
+    <div className="flex flex-row ml-4">
+      <div className={"flex-none"}>
+        <NavItem to={PageUrls.home()} className={"font-bold"}>
+          <img alt="logo" className="w-64" src={ logoimg }></img>
+        </NavItem>
       </div>
-    </nav>
+      <nav className="flex flex-col justify-center  ml-4 w-full">
+        <div className="flex justify-between border-b-4 border-solid border-accent">
+          <div className={"flex text-center color-text-shadow"}>
+            <NavItem className="color-text" to={PageUrls.home()}>PAST EVENTS</NavItem>
+            <NavItem className="color-text" to={PageUrls.home()}>PARTICIPATING SCHOOLS</NavItem>
+            <NavItem className="color-text" to={PageUrls.home()}>SPONSORS</NavItem>
+          </div>
+          <div className={"flex-none color-text"}>
+            <NavItem to={"https://github.com/joint-jam"}><FontAwesomeIcon icon={faGithub}/></NavItem>
+            <NavItem to={PageUrls.home()}><FontAwesomeIcon icon={faItchIo}/></NavItem>
+            <NavItem to={PageUrls.home()}><FontAwesomeIcon icon={faDiscord}/></NavItem>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
