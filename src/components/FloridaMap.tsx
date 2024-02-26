fb4bbc74672ecc9e857cc970f45d89fe6594f537
Alec Tremblay <84729196+BillyJoelsNightmareExplosion@users.noreply.github.com>
2024-02-26 17:51:47 -0500

    adds labels to pins on schoolmap

--- src/components/FloridaMap.tsx
+++ src/components/FloridaMap.tsx
@@ -1,20 +1,27 @@
 import styles from "@website/components/FloridaMap.module.css";
-import { School, Schools } from "@website/data/Schools";
+import type { School } from "@website/data/Schools";
+import { Schools } from "@website/data/Schools";
 import { Link } from "react-router-dom";
 
-const SchoolPin = ({school, size}: {school: School, size: number}) => {
+const SchoolPin = ({ school, size }: {school: School; size: number}) => {
   return (
-    <Link to={`/schools#${school.key}`} className={styles.schoolPin} style={{
-      "--color1": school.colors[0],
-      "--color2": school.colors[1],
-      "--x": `${school.mapPos[0] * size}px`,
-      "--y": `${school.mapPos[1] * size}px`,
-      "--size": `${size / 20}px`,
-    } as React.CSSProperties}></Link>
+    <div>
+      <Link to={`/schools#${school.key}`} className={styles.schoolPin} style={{
+        "--color1": school.colors[0],
+        "--color2": school.colors[1],
+        "--x": `${school.mapPos[0] * size}px`,
+        "--y": `${school.mapPos[1] * size}px`,
+        "--size": `${size / 20}px`,
+      } as React.CSSProperties}></Link>
+      <h2 className={styles.schoolPinLabel} style={{
+        "--x": `${school.mapPos[0] * size + size / 8 }px`,
+        "--y": `${school.mapPos[1] * size - size / 28}px`,
+      } as React.CSSProperties}>{school.name}</h2>
+    </div>
   );
 }
 
-export const FloridaMap = ({size}: {size: number}) => {
+export const FloridaMap = ({ size }: {size: number}) => {
   return (
     <div className={"relative"}>
       <div className={styles.floridaMap} style={{
