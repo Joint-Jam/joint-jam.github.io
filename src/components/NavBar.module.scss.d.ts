b4c70a7899547cfbb4f4acbfc8bf2c263eba675f
Dion Tryban <github@diontryban.com>
2024-03-01 20:47:33 -0500

    Use SCSS and make navbar work on smaller screens

---
+++ src/components/NavBar.module.scss.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly "centerText": string;
+  readonly "icons": string;
+  readonly "logo": string;
+  readonly "nav": string;
+  readonly "navContent": string;
+  readonly "navItem": string;
+};
+export = styles;
+
