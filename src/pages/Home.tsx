515b23b45fe0a535e77b6c01db86b206b451540d
alectrem <tremblayalec14@gmail.com>
2024-03-04 23:55:14 -0500

    changes home body

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,11 +13,11 @@ export const Home = () => {
         <div className={styles.left}>
           <h1><span className="italic">48 Hours...</span></h1>
           <p className="mb-4 text-2xl text-white">
-          ...is how much time student game developers at universities across Florida have to make an entire video game each year. Often, the code is terrible. Even the cut corners are visible, and low-rez. The designs make little sense. But out of thin air... <span className="striped-text">a video game.</span>
+          ...is how much time student game developers at universities across Florida have to make an entire video game each year. Often, the code is terrible. Even the cut corners are visible, and low-rez. The designs can make little sense.
+            <br></br><br></br>
+          But out of thin air... <span className="striped-text">a video game.</span>
             <br></br><br></br>
           Joint Jam '24 is a combined game jam between the game development clubs at USF, UCF, FSU, ERAU, and UF for all game developers! 
-            <br></br><br></br>
-          For the last three years, Joint Jam has been the competition where students all across Florida have showed their skills and tested their mettle.
             <br></br><br></br>
           So far:
             <ul>
