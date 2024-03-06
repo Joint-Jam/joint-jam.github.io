0669d620355237012f88e1988b9c4c498a0ab892
Dion Tryban <github@diontryban.com>
2024-03-06 17:08:00 -0500

    Add previous event data

--- src/data/Events.ts
+++ src/data/Events.ts
@@ -12,6 +12,18 @@ export const Events: Event[] = [
     end: new Date("2024-03-31T18:00"),
     itchUrl: "https://itch.io/jam/joint-jam-2024/",
   },
+  {
+    name: "Joint Jam 2023",
+    start: new Date("2023-03-31T19:00"),
+    end: new Date("2023-04-02T19:00"),
+    itchUrl: "https://itch.io/jam/joint-jam-2023/",
+  },
+  {
+    name: "Joint Jam 2022",
+    start: new Date("2022-04-01T19:00"),
+    end: new Date("2022-04-3T19:00"),
+    itchUrl: "https://itch.io/jam/florida-joint-jam",
+  },
 ];
 
 export const CurrentEvent: Event = Events[0];
