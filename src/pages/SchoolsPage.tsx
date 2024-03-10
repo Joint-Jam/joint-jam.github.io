390b9986dbe20c45ae447709fb951d98135dce0e
Exanite <exanite78012@gmail.com>
2024-03-10 19:13:50 -0400

    Refactor code and experiment with coloring

--- src/pages/SchoolsPage.tsx
+++ src/pages/SchoolsPage.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export const SchoolsPage = () => {
   return (
-    <div>
+    <div className={"bg-neutral-950"}>
       <Heading>Participating Schools</Heading>
       {Schools.sort((a, b) => a.club.name.localeCompare(b.club.name)).map((school) => (
         <HeadingLevel key={school.key}>
@@ -12,24 +12,25 @@ export const SchoolsPage = () => {
             id={school.key}
             className={"flex flex-row pt-8"}
           >
-            <div className={"relative flex aspect-square w-48 p-2"}>
-              <div className={"absolute inset-0 bg-[#1c2e30] rounded-l-full -z-10"}/>
-              <div className={"flex grow bg-[#67abb4] p-1 rounded-full"}>
-                <div className={"flex grow bg-neutral-950 rounded-full"}>
-                  <img
-                    className={"w-full aspect-square rounded-full"}
-                    src={school.club.logo}
-                    alt={school.club.name}
-                  />
+            <div className={"relative flex aspect-square w-48"}>
+              <div className={"flex grow bg-[#1c2e30] rounded-l-full p-2"}>
+                <div className={"flex grow bg-[#67abb4] p-1 rounded-full"}>
+                  <div className={"flex grow bg-neutral-900 rounded-full"}>
+                    <img
+                      className={"w-full aspect-square rounded-full"}
+                      src={school.club.logo}
+                      alt={school.club.name}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
 
             <div
-              className={"flex flex-row bg-neutral-950 flex-grow"}
+              className={"flex flex-row bg-neutral-900 flex-grow"}
             >
               <div
-                className={"flex flex-col bg-neutral-950 p-4"}
+                className={"flex flex-col bg-neutral-900 p-4"}
               >
                 <Heading className={"text-6xl"}>{school.club.name}</Heading>
                 <p>{school.name}</p>
