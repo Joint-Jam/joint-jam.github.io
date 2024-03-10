bb838e62b67bfa72e42cd393220679841c7fd0cb
Exanite <exanite78012@gmail.com>
2024-03-10 19:04:09 -0400

    Implement potential school design (this also edits the images to have full backgrounds)

--- src/pages/SchoolsPage.tsx
+++ src/pages/SchoolsPage.tsx
@@ -1,9 +1,6 @@
 import { Heading, HeadingLevel } from "@ariakit/react";
 import { Schools } from "@website/data/Schools";
-import clsx from "clsx";
 import { Link } from "react-router-dom";
-
-import styles from "./SchoolsPage.module.scss";
 
 export const SchoolsPage = () => {
   return (
@@ -13,31 +10,42 @@ export const SchoolsPage = () => {
         <HeadingLevel key={school.key}>
           <div
             id={school.key}
-            className={styles.school}
+            className={"flex flex-row pt-8"}
           >
-            <img
-              className={styles.logo}
-              src={school.club.logo}
-              alt={school.club.name}
-            />
-
-            <div
-              className={styles.left}
-            >
-              <Heading>{school.club.name}</Heading>
-              <p>{school.name}</p>
+            <div className={"relative flex aspect-square w-48 p-2"}>
+              <div className={"absolute inset-0 bg-[#1c2e30] rounded-l-full -z-10"}/>
+              <div className={"flex grow bg-[#67abb4] p-1 rounded-full"}>
+                <div className={"flex grow bg-neutral-950 rounded-full"}>
+                  <img
+                    className={"w-full aspect-square rounded-full"}
+                    src={school.club.logo}
+                    alt={school.club.name}
+                  />
+                </div>
+              </div>
             </div>
 
-            <div className={styles.expand}/>
-
             <div
-              className={styles.links}
+              className={"flex flex-row bg-neutral-950 flex-grow"}
             >
-              {school.club.urls.map(url => (
-                <Link key={url.name} to={url.url} className={clsx(styles.link, "striped-link")}>
-                  {url.icon} {url.name}
-                </Link>
-              ))}
+              <div
+                className={"flex flex-col bg-neutral-950 p-4"}
+              >
+                <Heading className={"text-6xl"}>{school.club.name}</Heading>
+                <p>{school.name}</p>
+              </div>
+
+              <div className={"flex-grow"}/>
+
+              <div
+                className={"flex flex-col bg-[#1c2e30] p-4 w-36 text-center"}
+              >
+                {school.club.urls.map(url => (
+                  <Link key={url.name} to={url.url} className={"text-xl pb-2"}>
+                    {url.icon} {url.name}
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
         </HeadingLevel>
