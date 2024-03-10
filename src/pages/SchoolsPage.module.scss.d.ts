3040714421a63ab7a594df39f8dfbd91231a3643
Dion Tryban <github@diontryban.com>
2024-03-10 15:03:05 -0400

    Add link styling

--- src/pages/SchoolsPage.module.scss.d.ts
+++ src/pages/SchoolsPage.module.scss.d.ts
@@ -1,2 +1,5 @@
+export declare const info: string;
+export declare const link: string;
+export declare const links: string;
 export declare const logo: string;
 export declare const school: string;
--- src/pages/SchoolsPage.tsx
+++ src/pages/SchoolsPage.tsx
@@ -1,4 +1,8 @@
+import { faDiscord, faItchIo } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Schools } from "@website/data/Schools";
+import clsx from "clsx";
 import { Link } from "react-router-dom";
 
 import styles from "./SchoolsPage.module.scss";
@@ -12,10 +16,23 @@ export const SchoolsPage = () => {
           <img src={school.club.logo} alt={school.club.name} className={styles.logo}/>
           <div>
             <h3>{school.club.name}</h3>
-            <p>{school.name}</p>
-            <div>
-              {school.club.websiteUrl && <Link to={school.club.websiteUrl} className={"striped-button"}>Website</Link>}
-              {school.club.discordUrl && <Link to={school.club.discordUrl} className={"striped-button"}>Discord</Link>}
+            <p className={styles.info}>{school.name}</p>
+            <div className={styles.links}>
+              {school.club.websiteUrl &&
+                <Link to={school.club.websiteUrl} className={clsx(styles.link, "striped-link")}>
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare}/>
+                </Link>
+              }
+              {school.club.discordUrl &&
+                <Link to={school.club.discordUrl} className={clsx(styles.link, "striped-link")}>
+                  <FontAwesomeIcon icon={faDiscord}/>
+                </Link>
+              }
+              {school.club.itchUrl &&
+                <Link to={school.club.itchUrl} className={clsx(styles.link, "striped-link")}>
+                  <FontAwesomeIcon icon={faItchIo}/>
+                </Link>
+              }
             </div>
           </div>
         </div>
