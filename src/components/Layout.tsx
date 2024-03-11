a9c7309e7f2c22db1a84bb46238ce00aee728afd
Exanite <exanite78012@gmail.com>
2024-03-10 20:04:24 -0400

    Show tailwind debug screens when in development

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,13 @@ import { Outlet } from "react-router-dom";
 
 import styles from "./Layout.module.scss";
 
+const isDevelopment = process.env.NODE_ENV == "development";
+
 export const Layout = () => {
   return (
-    <div>
+    <div className={isDevelopment ? "debug-screens" : ""}>
       {/* TODO: Remove the dev banner before shipping the website. */}
-      {(process.env.NODE_ENV !== "development") && (
+      {!isDevelopment && (
         <div className={styles.devBanner}>
           This Webpage is still in development. Some info may be incorrect or missing. Please check back later.
         </div>
