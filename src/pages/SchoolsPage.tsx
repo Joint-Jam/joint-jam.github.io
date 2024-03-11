e7e4ec3c10162824905c881bac7696a840e5cc00
Exanite <exanite78012@gmail.com>
2024-03-10 20:41:10 -0400

    Ensure schools page scales at all resolutions

--- src/pages/SchoolsPage.tsx
+++ src/pages/SchoolsPage.tsx
@@ -12,8 +12,8 @@ export const SchoolsPage = () => {
             id={school.key}
             className={"flex flex-row pt-8"}
           >
-            <div className={"relative flex aspect-square w-48"}>
-              <div className={"flex grow bg-primary-700 rounded-l-full p-2"}>
+            <div className={"flex flex-shrink-0 bg-neutral-950 md:bg-transparent"}>
+              <div className={"flex grow bg-primary-700 rounded-br-3xl md:rounded-l-full p-2 aspect-square w-24 h-24 md:w-56 md:h-56"}>
                 <div className={"flex grow bg-primary-500 p-1 rounded-full"}>
                   <img
                     className={"w-full aspect-square rounded-full"}
@@ -30,13 +30,13 @@ export const SchoolsPage = () => {
               <div
                 className={"flex flex-col p-4"}
               >
-                <Heading className={"text-6xl"}>{school.club.name}</Heading>
-                <p>{school.name}</p>
-                <ul className={"pt-4"}>
+                <Heading className={"text-2xl xs:text-3xl sm:text-4xl md:text-5xl leading-5"}>{school.club.name}</Heading>
+                <p className={"pt-1 text-xs sm:text-sm sm:text-md md:text-lg"}>{school.name}</p>
+                <ul className={"pt-3 md:pt-5"}>
                   {school.club.urls.map(url => (
                     <ul key={url.name}>
-                      <Link to={url.url} className={"text-lg pb-2"}>
-                        {url.icon} {url.name}
+                      <Link to={url.url} className={"text-xs sm:text-sm sm:text-md md:text-lg pb-2"}>
+                        <span className={"pr-1"}>{url.icon}</span> {url.name}
                       </Link>
                     </ul>
                   ))}
