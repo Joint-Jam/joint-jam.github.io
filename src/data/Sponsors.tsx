d60f8dde15e2bd51c15b6dc7b5f925e6584d043e
Exanite <exanite78012@gmail.com>
2024-03-24 18:31:29 -0400

    Add sponsors data and initial page design

---
+++ src/data/Sponsors.tsx
@@ -0,0 +1,86 @@
+import { faItchIo } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import iconStyles from "@website/styles/Icon.module.scss";
+import type { ReactNode } from "react";
+
+export interface Url {
+  name: string;
+  icon: ReactNode;
+  url: string;
+}
+
+export interface Sponsor {
+  key: string;
+  name: string;
+  logo: string;
+  website: Url;
+  prizes: Url[];
+}
+
+export const Sponsors: Sponsor[] = [
+  {
+    key: "pizza-doggy",
+    name: "Pizza Doggy",
+    logo: "https://img.itch.zone/aW1nLzE1MTUzMjczLnBuZw==/80x80%23/VTGZk0.png",
+    website: {
+      name: "Website",
+      icon: <FontAwesomeIcon className={iconStyles.icon} icon={faArrowUpRightFromSquare}/>,
+      url: "https://pizzadoggy.itch.io/",
+    },
+    prizes: [
+      {
+        name: "PSX Kidnapper Van x3",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faItchIo}/>,
+        url: "https://pizzadoggy.itch.io/kidnapper-van",
+      },
+      {
+        name: "Retro Fps Kit x3",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faItchIo}/>,
+        url: "https://pizzadoggy.itch.io/kidnapper-van",
+      },
+      {
+        name: "PSX Nature Mega Pack x3",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faItchIo}/>,
+        url: "https://pizzadoggy.itch.io/kidnapper-van",
+      },
+      {
+        name: "PSX Mega Pack x3",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faItchIo}/>,
+        url: "https://pizzadoggy.itch.io/kidnapper-van",
+      },
+      {
+        name: "PSX Bunkers x3",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faItchIo}/>,
+        url: "https://pizzadoggy.itch.io/kidnapper-van",
+      },
+    ],
+  },
+  {
+    key: "we-love-indies",
+    name: "We Love Indies",
+    logo: "https://www.weloveindies.com/_default_upload_bucket/dyn-wli-logo-light.png",
+    website: {
+      name: "Website",
+      icon: <FontAwesomeIcon className={iconStyles.icon} icon={faArrowUpRightFromSquare}/>,
+      url: "https://pizzadoggy.itch.io/",
+    },
+    prizes: [
+      {
+        name: "Sound Bundle B x1 (First place)",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faArrowUpRightFromSquare}/>,
+        url: "https://www.weloveindies.com/en/pricing",
+      },
+      {
+        name: "Music Bundle A x1 (First place)",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faArrowUpRightFromSquare}/>,
+        url: "https://www.weloveindies.com/en/pricing",
+      },
+      {
+        name: "Sound Bundle A x10 (Follow up)",
+        icon: <FontAwesomeIcon className={iconStyles.icon} icon={faArrowUpRightFromSquare}/>,
+        url: "https://www.weloveindies.com/en/pricing",
+      },
+    ],
+  },
+];
--- src/pages/SponsorsPage.tsx
+++ src/pages/SponsorsPage.tsx
@@ -1,7 +1,58 @@
+import { Heading, HeadingLevel } from "@ariakit/react";
+import { Sponsors } from "@website/data/Sponsors.tsx";
+import { Link } from "react-router-dom";
+
 export const SponsorsPage = () => {
   return (
     <div>
-      Sponsors :D
+      <Heading className={"text-4xl xs:text-5xl sm:text-6xl md:text-7xl m-auto pt-4"}>This Year<span className={"font-extrabold"}>'</span>s Sponsors</Heading>
+      {Sponsors.sort((a, b) => a.name.localeCompare(b.name)).map((sponsor) => (
+        <HeadingLevel key={sponsor.key}>
+          <div
+            id={sponsor.key}
+            className={"flex flex-row pt-8"}
+          >
+            <div className={"flex flex-shrink-0 bg-neutral-950 md:bg-transparent"}>
+              <div className={"flex grow bg-primary-700 rounded-br-3xl md:rounded-l-full p-2 aspect-square w-24 h-24 md:w-56 md:h-56"}>
+                <div className={"flex grow bg-primary-500 p-1 rounded-full"}>
+                  <img
+                    className={"w-full aspect-square rounded-full"}
+                    src={sponsor.logo}
+                    alt={sponsor.name}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div
+              className={"flex flex-row bg-neutral-950 flex-grow"}
+            >
+              <div
+                className={"flex flex-col p-4"}
+              >
+                <Heading className={"text-2xl xs:text-3xl sm:text-4xl md:text-5xl leading-5"}>{sponsor.name}</Heading>
+                <Link to={sponsor.website.url} className={"text-xs sm:text-sm sm:text-md md:text-lg pb-3 pt-1"} target={"_blank"}>
+                  <span className={"pr-1"}>{sponsor.website.icon}</span> {sponsor.website.name}
+                </Link>
+                <HeadingLevel>
+                  <Heading className={"xs:text-1xl sm:text-2xl md:text-3xl"}>
+                    Prizes
+                  </Heading>
+                  <ul>
+                    {sponsor.prizes.map(url => (
+                      <ul key={url.name}>
+                        <Link to={url.url} className={"text-xs sm:text-sm sm:text-md md:text-lg pb-2"} target={"_blank"}>
+                          <span className={"pr-1"}>{url.icon}</span> {url.name}
+                        </Link>
+                      </ul>
+                    ))}
+                  </ul>
+                </HeadingLevel>
+              </div>
+            </div>
+          </div>
+        </HeadingLevel>
+      ))}
     </div>
   );
 }
