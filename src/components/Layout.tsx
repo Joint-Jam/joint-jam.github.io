f548840e7fa7db0e8fee83c1a63f7c726c8db530
Exanite <exanite78012@gmail.com>
2024-03-24 18:51:53 -0400

    Remove in-development banner

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,12 +9,6 @@ const isDevelopment = process.env.NODE_ENV == "development";
 export const Layout = () => {
   return (
     <div className={isDevelopment ? "debug-screens" : ""}>
-      {/* TODO: Remove the dev banner before shipping the website. */}
-      {!isDevelopment && (
-        <div className={styles.devBanner}>
-          This Webpage is still in development. Some info may be incorrect or missing. Please check back later.
-        </div>
-      )}
       <header>
         <NavBar/>
       </header>
